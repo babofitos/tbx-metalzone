aafe6c25784d265ce53bc45b40f5880672d45fed
babofitos <tehchoi@gmail.com>
2014-11-29 21:51:23 -0500

    refactor

--- terrabattlex.js
+++ terrabattlex.js
@@ -2,7 +2,7 @@
 // @name        metal zone
 // @namespace   https://github.com/babofitos/tbx-metalzone.git
 // @include     http://www.terrabattlex.com/
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -39,96 +39,55 @@ function convertNumToDay(day) {
   return days[day];
 }
 
-function convertHour(hour) {
-  if (hour > 12) {
-    hour = hour - 12;
-    return hour.toString() + 'PM';
+function convert12Hour(hour) {
+  return ((hour + 11) % 12 + 1);
+}
+
+function calcPeriod(hour) {
+  if (hour >= 0 && hour < 12) {
+    return 'AM';
   } else {
-    return hour.toString() + 'AM';
+    return 'PM';
   }
 }
 
-function calcNextHour(dayAndHour) {
-  var day = dayAndHour[0];
-  var hourAndPeriod = separateHourFromPeriod(dayAndHour[1]);
-  var hour = hourAndPeriod[0];
-  var period = hourAndPeriod[1];
-
-  hour = calc24Hour(hour, period);
-  hour += 1;
-  period = calcPeriod(hour, period);
-  if (hour == 24) {
-    hour = 12;
-    dayAndHour[0] = days[loopDay(days.indexOf(day))];
-  } else if (hour > 23) {
-    hour -= 24;
-    dayAndHour[0] = days[loopDay(days.indexOf(day))];
-  } else if (hour > 12) {
-    hour -= 12;
-  } 
-  dayAndHour[1] = hour.toString() + period;
-
-  return dayAndHour;
-}
-
-function calc24Hour(hour, period) {
-  if (period == 'PM') {
-    hour += 12;
-  
-  }
-  return hour;
-}
-
-function calcPeriod(hour, period) {
-  if (hour == 12 || hour == 24) {
-    period = period == 'AM' ? 'PM' : 'AM';
-  }
-
-  return period;
-}
-
-function separateHourFromPeriod(hourAndPeriod) {
-  var divider = hourAndPeriod.indexOf('A');
-  divider = divider > -1 ? divider : hourAndPeriod.indexOf('P');
-  
-  var hour = +hourAndPeriod.slice(0, divider);
-  return [hour, hourAndPeriod.slice(divider)];
-}
-
-function loopDay(dayIndex) {
+function calcNextDay(dayIndex) {
   dayIndex += 1;
-
-  if (dayIndex == days.length) {
-    return 0;
-  }
-  return dayIndex;
+  
+  return dayIndex % days.length;
 }
 
-function getCurrentTime() {
-  var date = new Date();
-  var day = convertNumToDay(date.getDay());
-  var hour = convertHour(date.getHours());
-
-  return [day, hour];
+function isNextDay(hour) {
+  return hour === 0;
 }
 
-function parseTime(time) {
-  var both = time.split(',');
-  both[1] = both[1].trim();
-
-  return both;
+function calcNextHour(hour) {
+  hour += 1;
+  
+  return hour % 24;
 }
 
 function setColor(td) {
   var html = td.innerHTML;
-  var zoneTime = parseTime(html);
-  var current = getCurrentTime();
-  var copyCurrent = current.slice(0);
-  var nextHour = calcNextHour(copyCurrent);
+  var zoneTime = html;
+  var date = new Date();
 
-  if ((zoneTime[0] == current[0]) && (zoneTime[1] == current[1])) {
+  var hour = date.getHours();
+  var current12Hour = convert12Hour(hour);
+  var period = calcPeriod(hour);
+  var day = convertNumToDay(date.getDay());
+
+  var nextHour = calcNextHour(hour);
+  var next12Hour = convert12Hour(nextHour);
+  var nextPeriod = calcPeriod(nextHour);
+  var nextDay = isNextDay(nextHour) ? convertNumToDay(calcNextDay(date.getDay())) : day;
+  
+  var timeAndPeriod = day + ', ' + current12Hour + period;
+  var nextTimeAndPeriod = nextDay + ', ' + next12Hour + nextPeriod;
+
+  if (zoneTime == timeAndPeriod) {
     td.style.color = 'green';
-  } else if ((zoneTime[0] == nextHour[0]) && (zoneTime[1] == nextHour[1])) {
+  } else if (zoneTime == nextTimeAndPeriod) {
     td.style.color = 'rgb(204, 204, 0)';
   }
 
